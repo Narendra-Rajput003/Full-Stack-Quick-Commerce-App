1206af79b230bdb0a46cd049957d5ef29f401545
Jadon Narendrasinh M <narendrarajput05007@gmail.com>
2024-09-29 20:39:50 +0530

    add next-auth

---
+++ src/lib/auth/authOptions.ts
@@ -0,0 +1,61 @@
+
+import GoogleProvider from "next-auth/providers/google"
+import { db } from "../db/db"
+import { users } from "../db/schema"
+
+
+
+export const authOptions={
+    providers:[
+        GoogleProvider({
+            clientId:process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
+            async profile({ given_name, family_name, email, sub, picture }: any, token: any){
+
+                const data = {
+                    fname: given_name,
+                    lname: family_name,
+                    email: email,
+                    provider: 'GOOGLE',
+                    external_id: sub,
+                    image: picture,
+                };
+
+               try {
+                const user=await db.insert(users).values(data).onConflictDoUpdate({target:users.email,set:data}).returning();
+
+                return {
+                    ...data,
+                    name: data.fname, 
+                    id: String(user[0]?.id), 
+                    role: user[0]?.role //
+                }
+
+               } catch (error) {
+                console.error("Error inserting user:", error); // Improved error logging
+                throw new Error("User insertion failed");
+               }
+
+
+                return {
+                    id:sub
+                }
+            }
+        })
+
+    ],
+    callbacks:{
+        session(data:any){
+            return data
+            
+        },
+        jwt({token,user}:{token:any,user:any}){
+           if(user){
+            token.role=user.role,
+            token.id=user.id
+           }
+           return token;
+        }
+        
+    }
+}
